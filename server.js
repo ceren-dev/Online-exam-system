e54a8246ef2266a111ef2c47e1427ab3c144fab4
Ceren Ünyeli <cerenunyeli2003@gmail.com>
2026-01-29 00:57:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 console.log("### BENIM SERVER DOSYAM CALISIYOR ###");
 
+
+
 const express = require("express");
 const sql = require("mssql/msnodesqlv8");
 
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+// SERVER
+app.listen(3000, () => {
+  console.log("Server 3000 portunda çalışıyor");
+});
 
 
 const config = {
   driver: "msnodesqlv8",
-  connectionString:
+  connectionString://
     "Driver={ODBC Driver 18 for SQL Server};" +
     "Server=DESKTOP-QUEAH06\\SQLEXPRESS;" +
     "Database=sinavsistemi;" +
@@ -66,15 +72,15 @@ app.get("/exam/:id/result", async (req, res) => {
       return res.status(400).json({ error: "student_id gerekli" });
     }
 
-    const result = await sql.query`
-      SELECT
-        COUNT(*) AS total_questions,
-        SUM(CASE WHEN is_correct = 1 THEN 1 ELSE 0 END) AS correct_count,
-        SUM(CASE WHEN is_correct = 0 THEN 1 ELSE 0 END) AS wrong_count
-      FROM StudentAnswer
-      WHERE exam_id = ${exam_id}
-        AND student_id = ${student_id}
-    `;
+   const result = await sql.query`
+  SELECT
+    COUNT(*) AS total_questions,
+    COALESCE(SUM(CASE WHEN is_correct = 1 THEN 1 ELSE 0 END), 0) AS correct_count,
+    COALESCE(SUM(CASE WHEN is_correct = 0 THEN 1 ELSE 0 END), 0) AS wrong_count
+  FROM StudentAnswer
+  WHERE exam_id = ${exam_id}
+    AND student_id = ${student_id}
+`;
 
     const stats = result.recordset[0];
 
@@ -95,7 +101,54 @@ app.get("/exam/:id/result", async (req, res) => {
   }
 });
 
-// SERVER
-app.listen(3000, () => {
-  console.log("Server 3000 portunda çalışıyor");
+app.post("/answer", async (req, res) => {
+  const { exam_id, student_id, question_id, selected_option } = req.body;
+
+  try {
+    console.log(" ANSWER GELDİ:", req.body);
+
+    const correctResult = await sql.query`
+      SELECT Q.correct_option, EQ.question_version
+      FROM ExamQuestion EQ
+      JOIN Question Q
+        ON Q.question_id = EQ.question_id
+       AND Q.version_no = EQ.question_version
+      WHERE EQ.exam_id = ${exam_id}
+        AND EQ.question_id = ${question_id}
+    `;
+
+    const correctOption =
+      correctResult.recordset[0].correct_option.trim().toUpperCase();
+
+    const selected =
+      selected_option.trim().toUpperCase();
+
+    const isCorrect = selected === correctOption ? 1 : 0;
+
+    await sql.query`
+      INSERT INTO StudentAnswer (
+        student_id,
+        exam_id,
+        question_id,
+        question_version,
+        selected_option,
+        is_correct
+      )
+      VALUES (
+        ${student_id},
+        ${exam_id},
+        ${question_id},
+        ${correctResult.recordset[0].question_version},
+        ${selected},
+        ${isCorrect}
+      )
+    `;
+
+    res.json({ success: true, isCorrect });
+  } catch (err) {
+    console.error(" ANSWER HATASI:", err);
+    res.status(500).json({ error: err.message });
+  }
 });
+
+
